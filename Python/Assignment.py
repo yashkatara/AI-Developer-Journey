2386c6e6a8e091e29918e4a6f3b974efa293a5e5
yashkatara <yash.katara@lifeasesolutions.com>
2026-09-08 16:02:50 +0530

    learning python

---
+++ Python/Assignment.py
@@ -0,0 +1,8 @@
+#print your name 10 times
+for i in range(10):
+    print('Yash Katara')
+    
+    print (('Yash Katara' + '\n')*10)
+    
+    name = input('Enter your name: ')
+    print('Hello ' + name + '!')
--- Python/code.py
+++ Python/code.py
@@ -1,1 +1,1 @@
-print ('hello world')
+print ('Yash Katara')
